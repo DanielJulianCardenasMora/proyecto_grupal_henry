cd2b3f048ef21ed0d579590ced45a328aa8d437e
danieljulian <danielcamora@hotmail.com>
2024-03-18 16:53:23 -0500

    Agregar estilos a detail

--- Front_end/src/Components/Detail/Detail.jsx
+++ Front_end/src/Components/Detail/Detail.jsx
@@ -1,5 +1,7 @@
 import style from './Detail.module.css'
 import fondo from '../../assets/Imagenes/Detail_fondo_aplicar.png'
+import boton from '../../assets/Imagenes/Boton_seguirViendo.png'
+import hover from '../../assets/Imagenes/Boton_seguirViendo_hover.png'
 
 
 
@@ -11,9 +13,22 @@ function Detail() {
   return (
     <div className={style.container}>
 
-      <h1 className={style.texto1}>wer</h1>
-      <h1 className={style.texto2}>werwer</h1>
-      <h1 className={style.texto3}>erter</h1>
+      <h1 className={style.texto1}>Fecha de lanzamiento:</h1>
+      <h1 className={style.texto2}>DETALLE</h1>
+      <h1 className={style.texto3}>TIENDA</h1>
+      <h1 className={style.texto4}>EN</h1>
+      <div className={style.texto5_cont}>
+        <h1 className={style.texto5}>C</h1>
+        <h1 className={style.texto5}>O</h1>
+        <h1 className={style.texto5}>L</h1>
+        <h1 className={style.texto5}>E</h1>
+        <h1 className={style.texto5}>C</h1>
+        <h1 className={style.texto5}>C</h1>
+        <h1 className={style.texto5}>I</h1>
+        <h1 className={style.texto5}>O</h1>
+        <h1 className={style.texto5}>N</h1>
+      </div>
+
 
       <div className={style.box_fondo}>
         <img className={style.fondo} src={fondo} />
@@ -29,7 +44,11 @@ function Detail() {
       
 
       <div className={style.box_derecha}>
-        <button>hola</button>
+        <div className={style.boton}>
+          {/* <img className={style.boton1} src={hover} /> */}
+          <img className={style.boton2} src={boton} />
+        </div>
+        
       </div>
 
 
