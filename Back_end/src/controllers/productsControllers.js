999f2c23328fec708018014426c4aeb2ddf55780
kebrito <kellybritomejia@gmail.com>
2024-03-19 22:10:29 -0400

    Creando Controllers y dando funcionalidad

--- Back_end/src/controllers/productsControllers.js
+++ Back_end/src/controllers/productsControllers.js
@@ -1,55 +1,69 @@
-const { Product } = require('../db')
-const axios = require('axios')
+const { Product } = require("../db");
+const axios = require("axios");
 
 const getProducts = async () => {
-    try {
-        const response = await axios.get('https://wearfashion-947fb-default-rtdb.firebaseio.com/products/products.json');
-        const getInfo = response.data.map(element => {
-            return {
-                id: element.id,
-                name: element.name,
-                image: element.image,
-                description: element.description,
-                price: element.price,
-                stock: element.stock,
-                genero: element.genero,
-                category: element.category,
-            };
-        });
-        return getInfo;
-
-    } catch (error) {
-        console.error("Error al obtener productos:", error);
-        throw error; // Propagar el error para que sea manejado por el código que llama a esta función
-    }
+  try {
+    const response = await axios.get(
+      "https://wearfashion-947fb-default-rtdb.firebaseio.com/products/products.json"
+    );
+    const getInfo = response.data.map((element) => {
+      return {
+        id: element.id,
+        name: element.name,
+        image: element.image,
+        description: element.description,
+        price: element.price,
+        stock: element.stock,
+        genero: element.genero,
+        category: element.category,
+      };
+    });
+    return getInfo;
+  } catch (error) {
+    console.error("Error al obtener productos:", error);
+    throw error; // Propagar el error para que sea manejado por el código que llama a esta función
+  }
 };
 
 const productsDataBase = async () => {
-    try {
-        const productsApi = await getProducts();
+  try {
+    const productsApi = await getProducts();
 
-        const existingProducts = await Product.findAll();
-        // console.log('Productos existentes en la base de datos:', existingProducts); // Registro de productos existentes en la base de datos
+    const existingProducts = await Product.findAll();
+    // console.log('Productos existentes en la base de datos:', existingProducts); // Registro de productos existentes en la base de datos
 
-        if (!existingProducts.length) {
-            const createdProducts = await Product.bulkCreate(productsApi);
-            //console.log('Productos creados en la base de datos:', createdProducts); // Registro de los productos creados en la base de datos
-            return createdProducts;
-        } else {
-            return existingProducts;
-        }
-    } catch (error) {
-        console.error('Error al procesar los productos en la base de datos:', error);
-        throw error;
+    if (!existingProducts.length) {
+      const createdProducts = await Product.bulkCreate(productsApi);
+      //console.log('Productos creados en la base de datos:', createdProducts); // Registro de los productos creados en la base de datos
+      return createdProducts;
+    } else {
+      return existingProducts;
     }
+  } catch (error) {
+    console.error(
+      "Error al procesar los productos en la base de datos:",
+      error
+    );
+    throw error;
+  }
 };
 
-
 const getProductDetail = async (id) => {
+  try {
+    const product = await Product.findByPk(id);
 
-}
+    if (!product) {
+      throw new Error(`No se encontró ningún producto con el ID ${id}`);
+    }
+
+    return product;
+  } catch (error) {
+    console.error("Error al obtener el detalle del producto:", error);
+    throw error;
+  }
+};
 
 module.exports = {
-    productsDataBase,
-    getProductDetail,
+  productsDataBase,
+  getProductDetail,
 };
