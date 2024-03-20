446910ac761d197e157c02e0ec8c324ff62a9044
danieljulian <danielcamora@hotmail.com>
2024-03-20 18:48:17 -0500

    Cards ordenadas en products

--- Front_end/src/Components/Card/card.jsx
+++ Front_end/src/Components/Card/card.jsx
@@ -4,47 +4,29 @@ import style from "./card.module.css";
 
 
 
+
+
+
+
+
 export const Card = ( {Id, Nombre, Imagen, Stock, Precio, Genero, Categoria}) => {
 
   return (
     <div className={style.box}>
       <div className={style.container}>
-
         <div className={style.imgRow}>
           <div className={style.imgContainer}>
             <img className='imgCard'src={Imagen} alt='image' />
           </div>
         </div>
 
-        <div className={style.qq}>
-          <div className={style.ee}>
+        <div className={style.texto}>
+          <div className={style.nombre}>
             <h1>{Nombre}</h1>
           </div>
         </div>
-
-
-        {/* <div className={style.cardcontainer}>
-          <div className={style.card}> */}
-
-        {/*           
-            {products.map(product => (
-              <div key={product.id}>
-                <p>Id: {product.id}</p>
-                <p>Nombre: {product.name}</p> 
-                <p>Imagen: {product.image}</p>
-                <p>Stock: {product.stock}</p>
-                <p>Precio: {product.price}</p>
-                <p>Género: {product.genero}</p>
-                <p>Categoría: {product.category}</p>
-              </div>
-            ))} */}
-
-            
-          {/* </div>
-        </div> */}
       </div>
-
-      </div>
+    </div>
   );
 }
 
