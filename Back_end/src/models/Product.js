0efc975970c55439108d15e1a6088dab0b7fc0cb
kebrito <kellybritomejia@gmail.com>
2024-03-20 19:07:39 -0400

    Creacion de validaciones

--- Back_end/src/models/Product.js
+++ Back_end/src/models/Product.js
@@ -1,43 +1,70 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
+const {
+  validateName,
+  validatePrice,
+  validateDescription,
+  validateStock,
+} = require("../utils/validacion");
 
 module.exports = (sequelize) => {
-    sequelize.define(
-        'Product', {
-        id: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            primaryKey: true,
-            unique: true,
+  sequelize.define(
+    "Product",
+    {
+      id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+        unique: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          customValidator(value) {
+            validateName(value);
+          },
         },
-        name: {
-            type: DataTypes.STRING,
-            allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          customValidator(value) {
+            validateDescription(value);
+          },
         },
-        description: {
-            type: DataTypes.STRING,
-            allowNull: true,
+      },
+      price: {
+        type: DataTypes.FLOAT,
+        allowNull: false,
+        validate: {
+          customValidator(value) {
+            validatePrice(value);
+          },
         },
-        price: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      stock: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        validate: {
+          customValidator(value) {
+            validateStock(value);
+          },
         },
-        image: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        stock: {
-            type: DataTypes.INTEGER,
-            allowNull: true,
-        },
-        category: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
-        genero: {
-            type: DataTypes.STRING,
-            allowNull: true,
-        },
+      },
+      category: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      genero: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
-        { timestamps: false }
-    );
+    { timestamps: false }
+  );
 };
