42e1f6beb89f41110fd4ec31e1741be70f455127
danieljulian <danielcamora@hotmail.com>
2024-03-20 16:17:47 -0500

    Estilo completo de products

--- Front_end/src/Components/Products/Products.jsx
+++ Front_end/src/Components/Products/Products.jsx
@@ -1,11 +1,20 @@
 import style from './Products.module.css'
+import imagen from '../../assets/Imagenes/Products_fondo_aplicar.png'
+
+
+
+
+
+
 
 
 export const Products = () => {
   return (
     <div className={style.container}>
       <div className={style.bg}>
-        <div className={style.imagen}></div>
+        <div className={style.imagen}>
+          <img className={style.imagen} src={imagen} />
+        </div>
         <div className={style.btnNav}></div>
       </div>
     </div>
