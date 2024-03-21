0c63348147748b219ae97469f7e7a1385b9f6e65
kebrito <kellybritomejia@gmail.com>
2024-03-20 20:58:44 -0400

    Resolviendo conflito model

--- Back_end/src/models/Product.js
+++ Back_end/src/models/Product.js
@@ -8,59 +8,24 @@ const {
 
 module.exports = (sequelize) => {
   sequelize.define(
-<<<<<<< HEAD
-    "Product",
-    {
-      id: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        primaryKey: true,
-        unique: true,
-=======
     'Product',
     {
       id: {
         type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
->>>>>>> d2a7a6712a98ca4bacedc1b4202854fde91d53be
       },
       name: {
         type: DataTypes.STRING,
         allowNull: false,
-<<<<<<< HEAD
-        validate: {
-          customValidator(value) {
-            validateName(value);
-          },
-        },
-      },
-      description: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          customValidator(value) {
-            validateDescription(value);
-          },
-        },
-=======
       },
       description: {
         type: DataTypes.STRING,
         allowNull: true,
->>>>>>> d2a7a6712a98ca4bacedc1b4202854fde91d53be
       },
       price: {
         type: DataTypes.FLOAT,
         allowNull: false,
-<<<<<<< HEAD
-        validate: {
-          customValidator(value) {
-            validatePrice(value);
-          },
-        },
-=======
->>>>>>> d2a7a6712a98ca4bacedc1b4202854fde91d53be
       },
       image: {
         type: DataTypes.STRING,
@@ -68,18 +33,6 @@ module.exports = (sequelize) => {
       },
       stock: {
         type: DataTypes.INTEGER,
-<<<<<<< HEAD
-        allowNull: false,
-        validate: {
-          customValidator(value) {
-            validateStock(value);
-          },
-        },
-      },
-      category: {
-        type: DataTypes.STRING,
-=======
->>>>>>> d2a7a6712a98ca4bacedc1b4202854fde91d53be
         allowNull: true,
       },
       genero: {
@@ -89,8 +42,4 @@ module.exports = (sequelize) => {
     },
     { timestamps: false }
   );
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> d2a7a6712a98ca4bacedc1b4202854fde91d53be
