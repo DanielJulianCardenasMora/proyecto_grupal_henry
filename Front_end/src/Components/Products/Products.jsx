7286dc8496c23b06d0ee060f18825547f2f171ab
danieljulian <danielcamora@hotmail.com>
2024-03-20 21:01:32 -0500

    Ajuste de codigo a products

--- Front_end/src/Components/Products/Products.jsx
+++ Front_end/src/Components/Products/Products.jsx
@@ -1,25 +1,11 @@
 import style from './Products.module.css'
 import imagen from '../../assets/Imagenes/Products_fondo_aplicar.png'
-import derecha from '../../assets/Imagenes/Products_flechaDer_aplicar.png'
-import izquierda from '../../assets/Imagenes/Products_flechaIzq_aplicar.png'
-import { getAllProducts } from "../../redux/actions/actions";
-import { useSelector, useDispatch } from "react-redux";
-import { useEffect } from 'react';
-
-
 
 
 
 
 export const Products = () => {
-  const gamesDB = useSelector((state) => state.gamesCreated)
-  useEffect(() => {
-    dispatch(getAllProducts())
-  }, [])
-  const dispatch = useDispatch()
 
-
-  
 
 
 
@@ -29,10 +15,7 @@ export const Products = () => {
         <div className={style.imagen}>
           <img className={style.imagen} src={imagen} />
         </div>
-        {/* <div className={style.btnNav}>
-          <img src={izquierda} />
-          <img src={derecha} />
-        </div> */}
+
       </div>
     </div>
   )
