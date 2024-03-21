585dc1ceb5993701c204690c1ce2ec81af627b02
danieljulian <danielcamora@hotmail.com>
2024-03-20 21:46:26 -0500

    Filtro de hombre

--- Front_end/src/Components/Products/Products.jsx
+++ Front_end/src/Components/Products/Products.jsx
@@ -1,13 +1,23 @@
 import style from './Products.module.css'
 import imagen from '../../assets/Imagenes/Products_fondo_aplicar.png'
+import { getAllProducts } from "../../redux/actions/actions";
+import { useDispatch } from "react-redux";
+import { useEffect } from 'react';
+
+
 
 
 
 
 export const Products = () => {
+  useEffect(() => {
+    dispatch(getAllProducts())
+  }, [])
+  const dispatch = useDispatch()
 
 
 
+  
 
   return (
     <div className={style.container}>
@@ -15,7 +25,6 @@ export const Products = () => {
         <div className={style.imagen}>
           <img className={style.imagen} src={imagen} />
         </div>
-
       </div>
     </div>
   )
