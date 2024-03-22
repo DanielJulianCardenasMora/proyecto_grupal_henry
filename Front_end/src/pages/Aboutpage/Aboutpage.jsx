27d82eb318be44d083e34d4c13bff18e75f94099
kebrito <kellybritomejia@gmail.com>
2024-03-21 21:43:52 -0400

    Fix Rutas de los componentes

--- Front_end/src/pages/Aboutpage/Aboutpage.jsx
+++ Front_end/src/pages/Aboutpage/Aboutpage.jsx
@@ -1,6 +1,6 @@
 
 import React from "react"
-import CardProfile from "../../Components/Cardprofile/Cardprofile"
+import CardProfile from "../../Components/cardprofile/CardProfile";
 import background from "../../assets/Imagenes/About_fondoInferior_aplicar.png"
 import figuras from "../../assets/Imagenes/About_figuras_aplicar.png"
 import style from './about.module.css'
