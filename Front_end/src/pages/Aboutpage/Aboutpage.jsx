6378c7804432c7edb61033815632c1c5385a711d
kebrito <kellybritomejia@gmail.com>
2024-03-21 20:42:11 -0400

    Corrigiendo la ruta del Index

--- Front_end/src/pages/Aboutpage/Aboutpage.jsx
+++ Front_end/src/pages/Aboutpage/Aboutpage.jsx
@@ -1,68 +1,67 @@
-import React from "react"
-import CardProfile from "../../Components/Cardprofile/CardProfile"
-import background from "../../assets/Imagenes/About_fondoInferior_aplicar.png"
-import figuras from "../../assets/Imagenes/About_figuras_aplicar.png"
-import style from './about.module.css'
-import { Nav } from "../../Components"
+import React from "react";
+import CardProfile from "../../Components/cardprofile/CardProfile";
+import background from "../../assets/Imagenes/About_fondoInferior_aplicar.png";
+import figuras from "../../assets/Imagenes/About_figuras_aplicar.png";
+import style from "./about.module.css";
+import { Nav } from "../../Components";
 
 export const AboutPage = () => {
-    return (
-      <>
-   
-        <img  className={style.figuras} src={figuras}/>
-        <h3 className={style.workteam}>Workteam</h3>
-        <div className={style.container}>
-          <CardProfile
-            nombre="Alejo Borracci"
-            localidad="Rosario, Santa Fe, Argentina"
-            linkedin="https://www.linkedin.com/in/alejo-borracci-2323a6199/"
-            github="https://github.com/alejoborracci21"
-          />
-          <CardProfile
-            nombre="Kelly Brito Mejia"
-            localidad="Rosario, Santa Fe, Argentina"
-            linkedin="https://www.linkedin.com/in/kelly-brito-mejia-1284b6289/"
-            github="https://github.com/kebrito"
-          />
-          <CardProfile
-            nombre="Daniel Julian Cardenas Mora"
-            localidad="Rosario, Santa Fe, Argentina"
-            linkedin="https://www.linkedin.com/in/danieljuliancardenasmora/"
-            github="https://github.com/DanielJulianCardenasMora"
-          />
-          <CardProfile
-            nombre="Ludmila Rosa Miranda "
-            localidad="Rosario, Santa Fe, Argentina"
-            linkedin="https://www.linkedin.com/ludmila-rosa-miranda"
-            github="https://github.com/Foggynoti0n"
-          />
-          <CardProfile
-            nombre="Luciano Gallinger"
-            localidad="Rosario, Santa Fe, Argentina"
-            linkedin="https://www.linkedin.com/in/luciano-gallinger-954a09183/"
-            github="https://github.com/LucianoGLR"
-          />
-          <CardProfile
-            nombre="Alejandro Serrano Herrera"
-            localidad="Rosario, Santa Fe, Argentina"
-            linkedin="https://www.linkedin.com/in/serranoh93/"
-            github="https://github.com/SerranoH93"
-          />
-          <CardProfile
-            nombre="Kevin Jeremias Barrios"
-            localidad="Rosario, Santa Fe, Argentina"
-            linkedin=" https://www.linkedin.com/in/kevinjbarrios/"
-            github="https://github.com/Jerebrrs"
-          />
-          <CardProfile
-            nombre="Shirley Balbuena"
-            localidad="Rosario, Santa Fe, Argentina"
-            linkedin="https://www.linkedin.com/in/shirley-balbuena-0b5bab295/"
-            github=" https://github.com/ShirleyBal"
-          />
+  return (
+    <>
+      <img className={style.figuras} src={figuras} />
+      <h3 className={style.workteam}>Workteam</h3>
+      <div className={style.container}>
+        <CardProfile
+          nombre="Alejo Borracci"
+          localidad="Rosario, Santa Fe, Argentina"
+          linkedin="https://www.linkedin.com/in/alejo-borracci-2323a6199/"
+          github="https://github.com/alejoborracci21"
+        />
+        <CardProfile
+          nombre="Kelly Brito Mejia"
+          localidad="Rosario, Santa Fe, Argentina"
+          linkedin="https://www.linkedin.com/in/kelly-brito-mejia-1284b6289/"
+          github="https://github.com/kebrito"
+        />
+        <CardProfile
+          nombre="Daniel Julian Cardenas Mora"
+          localidad="Rosario, Santa Fe, Argentina"
+          linkedin="https://www.linkedin.com/in/danieljuliancardenasmora/"
+          github="https://github.com/DanielJulianCardenasMora"
+        />
+        <CardProfile
+          nombre="Ludmila Rosa Miranda "
+          localidad="Rosario, Santa Fe, Argentina"
+          linkedin="https://www.linkedin.com/ludmila-rosa-miranda"
+          github="https://github.com/Foggynoti0n"
+        />
+        <CardProfile
+          nombre="Luciano Gallinger"
+          localidad="Rosario, Santa Fe, Argentina"
+          linkedin="https://www.linkedin.com/in/luciano-gallinger-954a09183/"
+          github="https://github.com/LucianoGLR"
+        />
+        <CardProfile
+          nombre="Alejandro Serrano Herrera"
+          localidad="Rosario, Santa Fe, Argentina"
+          linkedin="https://www.linkedin.com/in/serranoh93/"
+          github="https://github.com/SerranoH93"
+        />
+        <CardProfile
+          nombre="Kevin Jeremias Barrios"
+          localidad="Rosario, Santa Fe, Argentina"
+          linkedin=" https://www.linkedin.com/in/kevinjbarrios/"
+          github="https://github.com/Jerebrrs"
+        />
+        <CardProfile
+          nombre="Shirley Balbuena"
+          localidad="Rosario, Santa Fe, Argentina"
+          linkedin="https://www.linkedin.com/in/shirley-balbuena-0b5bab295/"
+          github=" https://github.com/ShirleyBal"
+        />
 
-          <img src={background} className={style.background} />
-        </div>
-      </>
-    );
-}
+        <img src={background} className={style.background} />
+      </div>
+    </>
+  );
+};
--- Front_end/src/pages/index.js
+++ Front_end/src/pages/index.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { AboutPage } from "./Aboutpage/Aboutpage";
 import { HomePage } from "./Homepage/Homepage";
-import {LandingPage} from "./Landingpage/Landingpage";
-import {DetailPage} from "./Detailpage/Detailpage"
+import { LandingPage } from "./Landingpage/Landingpage";
+import { DetailPage } from "./Detailpage/Detailpage";
 import { CreatePage } from "./Createpage/Createpage";
 import { ProductsPage } from "./Productspage/Productspage";
-export {AboutPage, LandingPage, HomePage, DetailPage, CreatePage, ProductsPage}
-
+export {
+  AboutPage,
+  LandingPage,
+  HomePage,
+  DetailPage,
+  CreatePage,
+  ProductsPage,
+};
