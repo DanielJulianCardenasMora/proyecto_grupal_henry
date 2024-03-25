ba9a4ba26b8e0b1a3f9787fcfb45f15f62018c18
alejoborracci21 <borraccialejo@hotmail.es>
2024-03-25 18:34:05 -0300

    Solucion cardprofile

--- Front_end/src/Components/index.js
+++ Front_end/src/Components/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import Detail from "./Detail/Detail";
 import { Landing } from "./Landing/Landing";
-
 import Create_form from './Create_form/Create_form'
 import { Products } from './Products/Products'
-import { CardProfile } from "./cardprofile/CardProfile";
-
-
+import { CardProfile } from "./Cardprofile/CardProfile";
 import Nav from "./Nav/Nav";
 
 export { Detail, Create_form, Landing, Products, Nav, CardProfile};
