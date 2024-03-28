276b99ea2104cdce090e5e641b142c76efceeab4
Ludmila <lurm98@gmail.com>
2024-03-28 12:26:36 -0300

    cart

--- Front_end/src/Components/Cart/Cart.jsx
+++ Front_end/src/Components/Cart/Cart.jsx
@@ -28,10 +28,58 @@ export const Cart = ( ) => {
             </div>
             <div className={style.price}><p>$10</p></div>
         </div>
-        <div className={style.cards}></div>
-        <div className={style.cards}></div>
-        <div className={style.cards}></div>
-        <div className={style.cards}></div>
+        <div className={style.cards}>
+        <div className={style.image}></div>
+            <div className={style.desc}>
+              <h4>Nombre del producto</h4>
+              <button>Delete</button>
+            </div>
+            <div className={style.count}>
+             <div className={style.buttons}>
+             <button>+</button><p>0</p><button>-</button>
+             </div>
+            </div>
+            <div className={style.price}><p>$10</p></div>
+        </div>
+        <div className={style.cards}>
+        <div className={style.image}></div>
+            <div className={style.desc}>
+              <h4>Nombre del producto</h4>
+              <button>Delete</button>
+            </div>
+            <div className={style.count}>
+             <div className={style.buttons}>
+             <button>+</button><p>0</p><button>-</button>
+             </div>
+            </div>
+            <div className={style.price}><p>$10</p></div>
+        </div>
+        <div className={style.cards}>
+        <div className={style.image}></div>
+            <div className={style.desc}>
+              <h4>Nombre del producto</h4>
+              <button>Delete</button>
+            </div>
+            <div className={style.count}>
+             <div className={style.buttons}>
+             <button>+</button><p>0</p><button>-</button>
+             </div>
+            </div>
+            <div className={style.price}><p>$10</p></div>
+        </div>
+        <div className={style.cards}>
+        <div className={style.image}></div>
+            <div className={style.desc}>
+              <h4>Nombre del producto</h4>
+              <button>Delete</button>
+            </div>
+            <div className={style.count}>
+             <div className={style.buttons}>
+             <button>+</button><p>0</p><button>-</button>
+             </div>
+            </div>
+            <div className={style.price}><p>$10</p></div>
+        </div>
     </div>
  
     </div>
