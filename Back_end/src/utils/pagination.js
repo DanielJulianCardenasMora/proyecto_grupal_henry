03fe26058a0f53dc48d03f73f4216872a018f888
danieljulian <danielcamora@hotmail.com>
2024-03-27 20:11:33 -0500

    Actualizacion a paginacion

--- Back_end/src/utils/pagination.js
+++ Back_end/src/utils/pagination.js
@@ -1,6 +1,6 @@
 function paginarDatos(datos, page, pageSize) {
     const currentPage = parseInt(page) || 1;
-    const productsPerPage = parseInt(pageSize) || 5;
+    const productsPerPage = parseInt(pageSize) || 4;
     const offset = (currentPage - 1) * productsPerPage;
     const paginatedData = datos.slice(offset, offset + productsPerPage);
     const totalPages = Math.ceil(datos.length / productsPerPage);
