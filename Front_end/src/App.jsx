f99c1ced74e285d9e13dae00fd8b3cf4470c5a2d
danieljulian <danielcamora@hotmail.com>
2024-04-01 18:48:02 -0500

    Agrego ruta a perfil de usuario

--- Front_end/src/App.jsx
+++ Front_end/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom';
-import { LandingPage, AboutPage, DetailPage, CreatePage, ProductsPage } from './pages';
+import { LandingPage, AboutPage, DetailPage, CreatePage, ProductsPage, UserProfilePage } from './pages';
 import './App.css'
 import Card from './Components/Card/card';
 import { Nav } from './Components';
@@ -24,6 +24,7 @@ function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/create" element={<CreatePage />} />
         <Route path="/cards" element={<Card />} />
+        <Route path="/myprofile" element={<UserProfilePage />} />
       </Routes>
       
     </>
