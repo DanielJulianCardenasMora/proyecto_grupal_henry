29df2b36bc14294004e6970cdcab078672164f42
Ludmi <lurm98@gmail.com>
2024-04-03 09:03:02 -0300

    submit

--- Front_end/src/Components/Cart/Cart.jsx
+++ Front_end/src/Components/Cart/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from "react-router-dom";
 import style from "./Cart.module.css";
 import  ItemCount  from './ItemCount';
@@ -6,17 +6,24 @@ import  ItemCount  from './ItemCount';
 
 
 const Cart = ({ carrito, agregarProducto }) => {
-
-  const [carritoItems, setCarritoItems] = useState(carrito.map(item => ({ ...item, quantity: 1 })));
   const totalInicial = carrito.reduce((total, item) => total + item.price * item.quantity, 0);
   const [totalCompra, setTotalCompra] = useState(totalInicial);
 
-  console.log('carrito:', carrito);
-  console.log('carritoItems:' , carritoItems);
-
+  useEffect(() => {
+  setTotalCompra(totalInicial)
+  }, [carrito])
+  
 
   const agregarItem = (item) => {
     agregarProducto([...carrito, { ...item, quantity: 1 }]);
+  };
+
+
+  const eliminarProducto = (item) => {
+    const filtrados = carrito.filter((p) => p.id !== item.id);
+    console.log(filtrados);
+    agregarProducto(filtrados);
+
   };
 
   const handleQuantityChange = (newQuantity, item) => {
@@ -27,22 +34,15 @@ const Cart = ({ carrito, agregarProducto }) => {
       return cartItem;
     });
     agregarProducto(updatedItems)
-    setCarritoItems(updatedItems);
+ 
     // Actualiza el total de la compra restando el precio del artículo eliminado
     const totalPrice = updatedItems.reduce((acc, curr) => acc + (curr.price * curr.quantity), 0);
     setTotalCompra(totalPrice);
   };
 
-  const eliminarProducto = (item) => {
-    const filtrados = carrito.filter((p) => p.id !== item.id);
-    console.log(filtrados);
-    agregarProducto(filtrados);
-    setCarritoItems(filtrados)
-  };
-
   const vaciarCarrito = () => {
     agregarProducto([]);
-    setCarritoItems([])
+
   };
 
   return (
@@ -56,12 +56,13 @@ const Cart = ({ carrito, agregarProducto }) => {
         {carrito.map((item, i) => (
           <div className={style.cards} key={i}>
             <div className={style.image}>
-              {item.image == null ? <p>Imagen no disponible</p> : item.image}
+              {item.images[0] == null ? <p>Imagen no disponible</p> :   <img src={ item.images[0]} alt="" />}
             </div>
             <div className={style.desc}>
               <h4>{item.name}</h4>
               <button onClick={() => eliminarProducto(item)}>Delete</button>
             </div>
+          
             <div className={style.count}>
               <ItemCount
                 stock={item.stock}
@@ -98,3 +99,96 @@ const Cart = ({ carrito, agregarProducto }) => {
 }; 
 
 export default Cart
+
+// const Cart = ({ carrito, agregarProducto }) => {
+
+//   const [carritoItems, setCarritoItems] = useState(carrito.map(item => ({ ...item, quantity: 1 })));
+//   const totalInicial = carrito.reduce((total, item) => total + item.price * item.quantity, 0);
+//   const [totalCompra, setTotalCompra] = useState(totalInicial);
+
+//   console.log('carrito:', carrito);
+//   console.log('carritoItems:' , carritoItems);
+
+
+//   const agregarItem = (item) => {
+//     agregarProducto([...carrito, { ...item, quantity: 1 }]);
+//   };
+
+//   const handleQuantityChange = (newQuantity, item) => {
+//     const updatedItems = carrito.map((cartItem) => {
+//       if (cartItem.id === item.id) {
+//         return { ...cartItem, quantity: newQuantity };
+//       }
+//       return cartItem;
+//     });
+//     agregarProducto(updatedItems)
+//     setCarritoItems(updatedItems);
+//     // Actualiza el total de la compra restando el precio del artículo eliminado
+//     const totalPrice = updatedItems.reduce((acc, curr) => acc + (curr.price * curr.quantity), 0);
+//     setTotalCompra(totalPrice);
+//   };
+
+//   const eliminarProducto = (item) => {
+//     const filtrados = carrito.filter((p) => p.id !== item.id);
+//     console.log(filtrados);
+//     agregarProducto(filtrados);
+//     setCarritoItems(filtrados)
+//   };
+
+//   const vaciarCarrito = () => {
+//     agregarProducto([]);
+//     setCarritoItems([])
+//   };
+
+//   return (
+//     <div className={style.boxCart}>
+//       <div className={style.content}>
+//         {carrito.length ? (
+//           <h3>Revisa tus compras!</h3>
+//         ) : (
+//           <p>No has seleccionado productos aún</p>
+//         )}
+//         {carrito.map((item, i) => (
+//           <div className={style.cards} key={i}>
+//             <div className={style.image}>
+//               {item.image == null ? <p>Imagen no disponible</p> : item.image}
+//             </div>
+//             <div className={style.desc}>
+//               <h4>{item.name}</h4>
+//               <button onClick={() => eliminarProducto(item)}>Delete</button>
+//             </div>
+//             <div className={style.count}>
+//               <ItemCount
+//                 stock={item.stock}
+//                 initial={1}
+//                 item={item}
+//                 agregarItem={() => agregarItem(item)}
+//                 onQuantityChange={(newQuantity) => handleQuantityChange(newQuantity, item)}
+//               />
+//             </div>
+//           </div>
+//         ))}
+//         {carrito.length ? (
+//           <div className={style.buy}>
+//             <div className={style.total}>
+//               <span>Total: ${totalCompra}</span>
+//             </div>
+//             <div className={style.buttonsDiv}>
+//               <button className={style.back}>INICIAR COMPRA</button>
+//               <button className={style.vaciar} type="button" onClick={() => vaciarCarrito(carrito)}>
+//                 Vaciar Carrito
+//               </button>
+//             </div>
+//           </div>
+//         ) : (
+//           <button className={style.back}>
+//             <Link className={style.link} to="/products">
+//               Ver productos disponibles
+//             </Link>
+//           </button>
+//         )}
+//       </div>
+//     </div>
+//   );
+// }; 
+
--- Front_end/src/Components/Cart/ItemCount.jsx
+++ Front_end/src/Components/Cart/ItemCount.jsx
@@ -2,19 +2,19 @@ import React, { useState } from 'react';
 import style from "./ItemCount.module.css";
 
 const ItemCount = ({ stock, initial, item, onQuantityChange }) => {
-  const [quantity, setQuantity] = useState(initial);
+  
 
   const addItem = () => {
-    if (quantity < stock) {
-      setQuantity(quantity + 1);
-      onQuantityChange(quantity + 1);
+    if (item.quantity < stock) {
+      item.quantity + 1;
+      onQuantityChange(item.quantity + 1);
     }
   };
 
   const deleteItem = () => {
-    if (quantity > initial) {
-      setQuantity(quantity - 1);
-      onQuantityChange(quantity - 1);
+    if (item.quantity > initial) {
+      item.quantity - 1;
+      onQuantityChange(item.quantity - 1);
     }
   };
 
@@ -22,11 +22,11 @@ const ItemCount = ({ stock, initial, item, onQuantityChange }) => {
     <div className={style.countContainter}>
       <div className={style.buttons}>
         <button onClick={deleteItem}>-</button>
-        <p>{quantity}</p>
+        <p>{item.quantity}</p>
         <button onClick={addItem}>+</button>
       </div>
       <div className={style.price}>
-        <p>${item.price * quantity}</p>
+        <p>${item.price * item.quantity}</p>
       </div>
     </div>
   );
