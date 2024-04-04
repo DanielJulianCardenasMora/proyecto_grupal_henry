3fb7123d49a55d9b47f0f9ed5e44c0fee50b5307
alejoborracci21 <borraccialejo@hotmail.es>
2024-04-04 20:34:58 -0300

    Deploy done

--- Back_end/config.js
+++ Back_end/config.js
@@ -5,6 +5,7 @@ const DB_PASSWORD = process.env.DB_PASSWORD || process.env.LOCAL_DB_PASSWORD
 const DB_NAME = process.env.DB_NAME || process.env.LOCAL_DB_NAME
 const DB_PORT = process.env.DB_PORT || process.env.LOCAL_DB_PORT
 const DB_HOST = process.env.DB_HOST || process.env.LOCAL_DB_HOST
+
 module.exports = {
     PORT,
     DB_NAME,
