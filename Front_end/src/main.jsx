fbdf84321bb4920d2bb6ee1d923f8da1666c0e4b
alejoborracci21 <borraccialejo@hotmail.es>
2024-04-04 01:38:46 -0300

    Done GoogleLogin

--- Front_end/src/main.jsx
+++ Front_end/src/main.jsx
@@ -3,13 +3,22 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Auth0Provider } from '@auth0/auth0-react';
 import store from './redux/store/store';
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Auth0Provider
+      domain="dev-arjzo5g27mo4l5dl.us.auth0.com"
+      clientId="6ZWyffX1nIXG5p3uHDULbuGp23NbInEJ"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Auth0Provider>
   </Provider>
 );
