acc9b0cac9ad95f072735becaf8acca21ca64788
Ludmi <lurm98@gmail.com>
2024-04-09 15:56:20 -0300

    estilos pagina principal dashboard

--- Front_end/src/pages/Dashboard/homeadmin.jsx
+++ Front_end/src/pages/Dashboard/homeadmin.jsx
@@ -1,11 +1,24 @@
 import style from './css/HomeAdmin.module.css'
+import Sidebar from "../../Components/Dashboard/sidebar"
 
 export default function HomeAdmin(){
     return (
         <div className={style.main}>
-            <h1>Hola home</h1>
+                    <Sidebar />
+<div className={style.dashCont}>
+<div className={style.boxes}>
+    <div className={style.box}>Aca va la cantidad de clientes</div>
+    <div className={style.box}>Aca va la cantidad de ordenes </div>
+    <div className={style.box}>Aca van las ganancias semanales</div>
+    <div className={style.box}>Aca va un calendario decorativo</div>
 
-            <h2>Calendario</h2>
+</div>
+
+<div className={style.content}>
+<div className={style.principal}>Aca se van a renderizar cards con las ordenes de compra</div>
+<div className={style.side}>Aca se van a renderizar los clientes/usuarios</div>
+</div>
+</div>
         </div>
     )
 } 
