636c696be6d37da0332f62e6f16bca3e493cb6d3
alejoborracci21 <borraccialejo@hotmail.es>
2024-04-10 17:22:07 -0300

    nav

--- Front_end/src/Components/Nav/Nav.jsx
+++ Front_end/src/Components/Nav/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import style from './nav.module.css';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
 import { useNavigate } from 'react-router-dom';
 
