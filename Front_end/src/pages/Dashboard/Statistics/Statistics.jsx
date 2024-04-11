2322d07cba03762dada5d06ba3005030b933da80
danieljulian <danielcamora@hotmail.com>
2024-04-11 14:52:44 -0500

    statistics dash

--- Front_end/src/pages/Dashboard/Statistics/Statistics.jsx
+++ Front_end/src/pages/Dashboard/Statistics/Statistics.jsx
@@ -1,9 +1,36 @@
 import style from '../css/Statistics.module.css'
-import Sidebar from "../../../Components/Dashboard/sidebar";
 
 const Statistics = () => {
   return (
-    <div>Statistics
+    <div className={style.main}>
+      <div className={style.titlebox}>
+        <h1 className={style.title}>Statistics</h1>
+        <h1 className={style.title}>Performance</h1>
+        <h1 className={style.title}>Incomes</h1>
+        <h1 className={style.title}>Results</h1>
+      </div>
+
+      <div className={style.scrollseccion}>
+        <div className={style.seccion1}>
+          <div>Total sales income</div>
+          <div>Earnings 40% basis</div>
+          <div>Total orders</div>
+        </div>
+   
+
+        <div className={style.seccion2}>
+          <div>Sales per month</div>
+          <div>Numer of sales per month</div>
+        </div>
+        <div className={style.seccion3}>
+          <div>product ranking - top 5 most saled</div>
+          <div>product ranking - total orders per product</div>
+        </div>
+        <div className={style.seccion4}>
+          <div>Location sales</div>
+          <div>Top 5 customers</div>
+        </div>
+      </div>
     
     </div>
   )
