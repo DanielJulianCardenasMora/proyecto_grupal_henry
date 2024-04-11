13884b14dbb72b98c96c2785483435875b75515a
kebrito <kellybritomejia@gmail.com>
2024-04-11 10:28:14 -0400

    Configurando nodemailer

--- Back_end/src/utils/emailers.js
+++ Back_end/src/utils/emailers.js
@@ -1,27 +1,38 @@
-// const nodemailer = require("nodemailer");
-// const { User, Order } = require("../db");
-// const getEmailTemplate = require("../template/getEmailTemplate");
-// const createTrans = () => {
-//   const transport = nodemailer.createTransport({
-//     host: "sandbox.smtp.mailtrap.io",
-//     port: 2525,
-//     auth: {
-//       user: "f6a91bab298409",
-//       pass: "5a55c3a8b0c443",
-//     },
-//   });
-//   return transport;
-// };
-// const sendMail = async (userCreatedDB) => {
-//   const transporter = createTrans();
-//   const htmltemplate = getEmailTemplate(userCreatedDB.name);
-//   const info = await transporter.sendMail({
-//     from: '"Wearfashion" <wemolde@gmail.com>',
-//     to: `${userCreatedDB.email}`,
-//     subject: `Hola ${userCreatedDB.name} Welcome to Wearfashion`,
-//     html: htmltemplate,
-//   });
-//   console.log("Message sent:%s", info.messageId);
-// };
+const nodemailer = require("nodemailer");
+const getEmailTemplate = require("../template/getEmailTemplate");
 
-// exports.sendMail = (userCreatedDB) => sendMail(userCreatedDB);
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  tls: { rejectUnauthorized: false },
+  auth: {
+    user: "marty.hauck19@ethereal.email",
+    pass: "Gf57EeZ1PDTeHCPrS7",
+  },
+});
+
+const sendMail = async (userCreatedDB) => {
+  // Verifica que el objeto userCreatedDB contenga la propiedad 'name'
+  if (!userCreatedDB || !userCreatedDB.name) {
+    throw new Error(
+      "The userCreatedDB object does not have the 'name' property"
+    );
+  }
+
+  const htmlTemplate = getEmailTemplate(userCreatedDB.name);
+
+  try {
+    // Send email
+    const info = await transporter.sendMail({
+      from: '"Wearfashion" <wemolde@gmail.com>',
+      to: userCreatedDB.email,
+      subject: `Welcome ${userCreatedDB.name} to Wearfashion`,
+      html: htmlTemplate,
+    });
+  } catch (error) {
+    console.error("Error sending email:", error);
+    throw error;
+  }
+};
+
+exports.sendMail = (userCreatedDB) => sendMail(userCreatedDB);
