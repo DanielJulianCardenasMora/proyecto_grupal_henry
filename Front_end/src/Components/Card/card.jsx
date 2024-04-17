948e4c5b5986bd2c9a6c895b70daca59021cf5f9
kevin <kevinbrrs96@gmail.com>
2024-04-17 16:24:14 -0300

    Se corrigio bug en frontend

--- Front_end/src/Components/Card/card.jsx
+++ Front_end/src/Components/Card/card.jsx
@@ -3,38 +3,38 @@ import style from "./card.module.css";
 
 
 
-export const Card = ( {Id, Nombre, Imagen, Stock, Precio, Genero, Categoria}) => {
+export const Card = ({ Id, Nombre, Imagen, Stock, Precio, Genero, Categoria }) => {
   return (
     <div className={style.box}>
       <Link to={`/detail/${Id}`}>
-      
-      <div className={style.container}>
-        <div className={style.imgRow}>
-          <div className={style.imgContainer}>
-            <img className='imgCard'src={Imagen} alt='image' />
-          </div>
-        </div>
-          
-        <div className={style.texto}>
-          <div className={style.nombre}>
-            <h1>{Nombre}</h1>
-          </div>
-        </div>
-          
 
-        <div className={style.texto2}>
-          <div className={style.precio}>
-            <h1>{`$${Precio}`}</h1>
+        <div className={style.container}>
+          <div className={style.imgRow}>
+            <div className={style.imgContainer}>
+              <img className='imgCard' src={Imagen} alt='image' />
+            </div>
           </div>
-        </div>
 
-        {/* <div className={style.texto3}>
+          <div className={style.texto}>
+            <div className={style.nombre}>
+              <h1>{Nombre}</h1>
+            </div>
+          </div>
+
+
+          <div className={style.texto2}>
+            <div className={style.precio}>
+              <h1>{`$${Precio}`}</h1>
+            </div>
+          </div>
+
+          {/* <div className={style.texto3}>
           <div className={style.comprar}>
             <h1>COMPRAR</h1>
           </div>
         </div> */}
 
-      </div>
+        </div>
       </Link>
     </div>
   );
