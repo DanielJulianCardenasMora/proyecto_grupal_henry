8e83ad29dd71b490eaed3171c2d1586de468d8db
Ludmi <lurm98@gmail.com>
2024-04-17 03:08:11 -0300

    styles

--- Front_end/src/Components/Detail/Detail.jsx
+++ Front_end/src/Components/Detail/Detail.jsx
@@ -5,7 +5,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from 'react'
 import { getProductDetail } from '../../redux/actions/actions'
 import shape from '../../assets/Imagenes/Detail_shape_aplicar.png'
-import ItemCount from '../Cart/ItemCount';
+
 
 function Detail(props) {
   const dispatch = useDispatch()
@@ -107,54 +107,36 @@ useEffect(() => {
   return (
     <div className={style.container}>
 
-      <h1 className={style.texto2}>DETAIL</h1>
-      <h1 className={style.texto3}>STORE</h1>
-      <h1 className={style.texto4}>IN</h1>
 
-      <div className={style.box_fondo}>
-        <img className={style.fondo} src={fondo} />
-      </div>
+ <div className= {style.content}>
+ <div className={style.producto}>
+ <div className={style.boton}>
+          <div className={buttonClass ? style.boton_img : style.boton_img_hover }>   <button className={style.action} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleBackClick}></button></div>
+         
+        </div>
+        <img src={images} alt=""  className={style.img1}/>  
 
-      <div className={style.producto}>
-        <img src={images} alt="" />  
-        <div className={style.shape}>
-          <img className={style.shape} src={shape} alt="" />  
+        <div className={style.des}>
+         <span>About this</span>
+          <br />
+          <p>    {product.description}</p>
+      
         </div>
-        <div className={style.texto5_cont}>
-          <h1 className={style.texto5}>C</h1>
-          <h1 className={style.texto5}>O</h1>
-          <h1 className={style.texto5}>L</h1>
-          <h1 className={style.texto5}>E</h1>
-          <h1 className={style.texto5}>C</h1>
-          <h1 className={style.texto5}>T</h1>
-          <h1 className={style.texto5}>I</h1>
-          <h1 className={style.texto5}>O</h1>
-          <h1 className={style.texto5}>N</h1>
-        </div>
+      
+
       </div>
 
 
 
       <div className={style.box_derecha}>
-      <div className={style.box2}>
-        <div className={style.difuminado2}>{description}</div>
-      </div>
-        <h1 className={style.name}>{name}</h1>
-        <div className={style.boxDetalle}>
-          <h1 className={style.detalle1}>Category</h1>
-          <h1 className={style.detalle2}>{genero}</h1>
-          <h1 className={style.detalle3}>${price}</h1>
-        </div>
-        
-      
-        <div className={style.boton}>
-          <div className={buttonClass ? style.boton_img : style.boton_img_hover }></div>
-          <div className={style.action} onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={handleBackClick}></div>
-        </div>
+      <h2>{name}</h2>
+      <span> ${price}</span>
+
+    
         <div className={style.buy}>
 
 
-        <select onChange={handleSizeChange} value={selectedSize}>
+        <select onChange={handleSizeChange} value={selectedSize} className={style.selectS}>
           <option value="all">SIZE</option>
         {sizeWithoutTotal?.map(([size]) => (
           <option key={size} value={size}>{size}</option>
@@ -169,11 +151,20 @@ useEffect(() => {
   )}
   
           <button
+          className={style.cart}
           type="button"
           onClick={() => selectProducts(product)}
           >Add to cart</button>
         </div>
+
+      <div className={style.sizes}>
+
       </div>
+        
+      
+
+      </div>
+ </div>
     </div>
   )
 }
